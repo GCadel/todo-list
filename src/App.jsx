1fb34ad157d26f36a9af0adb9c3ad4195c3cef05
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-07-30 18:02:37 +0900

    Updated TodoForm to update todoList state in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 function App() {
-  const [newTodo, setNewTodo] = useState('Complete week 3 coding assignment');
+  const [todoList, setTodoList] = useState([]);
+  function addTodo(title) {
+    const newTodo = { title: title, id: Date.now() };
+    setTodoList([...todoList, newTodo]);
+  }
   return (
     <div>
       <h1>My Todos</h1>
-      <TodoForm />
-      <p>{newTodo}</p>
+      <TodoForm onAddTodo={addTodo} />
+
       <TodoList />
     </div>
   );
