efb1e54e8f63c221f5c635636dd79089a2cea280
George <47188474+gcmoony@users.noreply.github.com>
2025-08-07 20:18:47 +0900

    Update TodoForm.jsx
    Removed unnecessary comments

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,21 +1,17 @@
 import { useRef } from 'react';
 
 export default function TodoForm({ onAddTodo }) {
-  // Using ref to get the DOM element
   const todoTitleInput = useRef('');
   function handleAddTodo(event) {
     event.preventDefault();
-    // Can target an item by the name attribute
     const title = event.target.title;
     onAddTodo(title.value);
-    // Clearing the input after submit
     title.value = '';
     todoTitleInput.current.focus();
   }
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      {/* Adding ref to the input element */}
       <input ref={todoTitleInput} name="title" type="text" id="todoTitle" />
       <button>Add Todo</button>
     </form>
